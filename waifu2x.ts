2efa7d7829cd25c93e35b89aa31018d540caac96
Tenpi <pie4chan@gmail.com>
2021-06-29 02:43:57 -0400

    fix

--- waifu2x.ts
+++ waifu2x.ts
@@ -276,7 +276,7 @@ export default class Waifu2x {
             return newDest
         }
         newDest = `${dest}_${i}`
-        while (fs.existsSync(newDest) && i < 10) {
+        while (fs.existsSync(newDest) || i < 10) {
             if (fs.existsSync(newDest)) {    
                 const settings = JSON.parse(fs.readFileSync(`${newDest}/settings.json`, "utf8"))
                 if (JSON.stringify(settings) === JSON.stringify(options)) {
