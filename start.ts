216c2ea9bf38dac6d101580b1e5d5e061f6b0075
Tenpi <pie4chan@gmail.com>
2021-11-12 13:40:45 -0500

    fix mac version

--- start.ts
+++ start.ts
@@ -6,6 +6,6 @@ import waifu2x from "./waifu2x"
     }
     const result = await waifu2x.upscaleVideo("./images/videos/paradise.mp4", "./images/videos/paradise2x.mp4", {scale: 1.1, speed: 2, pitch: false, framerate: 1}, progress)
     const result = await waifu2x.upscaleGIF("./images/gifs/fbi.gif", "./images/gifs/fbi2x.gif", {scale: 1.1, parallelFrames: 2}, progress)*/
-    const result = await waifu2x.upscaleImage("./images/laffey2.jpg", "./images/upscale/laffey22x.jpg")
+    const result = await waifu2x.upscaleImage("./images/laffey2.jpg", "./images/upscale/laffey22x.jpg").catch((e) => console.log(e))
     console.log(result)
 })()
