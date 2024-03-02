2f4c30450486478206c3fd52dd97cb7e8e770594
Tenpi <pie4chan@gmail.com>
2024-03-02 14:56:45 -0500

    fix

--- start.ts
+++ start.ts
@@ -5,7 +5,8 @@ const start = async () => {
     const progress = (current: number, total: number) => {
         console.log(`${current}/${total}`)
     }
-    const result = await waifu2x.upscalePDF("./images/pdfs/pdf.pdf", "./images/pdfs/pdf2.pdf", {downscaleHeight: 1000})
+    //const result = await waifu2x.upscalePDF("./images/pdfs/pdf.pdf", "./images/pdfs/pdf2.pdf", {downscaleHeight: 1000})
+    const result = await waifu2x.upscaleImage("./images/img.png", "./images/upscale/img2x.png", {upscaler: "real-esrgan", scale: 4})
     console.log(result)
 }
 start()
